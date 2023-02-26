593e604b1cc7af4fa8c16f5fcc859301eae3fc1a
Amit <amit.naikwadi84@gmail.com>
2023-02-26 14:47:28 +0530

    file corrected

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,12 +36,14 @@ tracer = trace.get_tracer(__name__)
 # AWS X-Ray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 app = Flask(__name__)
 # Honeycomb Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+#AWS X-ray
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
