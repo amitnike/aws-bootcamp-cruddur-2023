5586b2efe57d195b9c584ba5656e6967db6508d7
Amit <amit.naikwadi84@gmail.com>
2023-03-06 20:11:16 +0000

    changes to add new auth module

--- node-jwt-validator/server.js
+++ node-jwt-validator/server.js
@@ -1,7 +1,7 @@
-const awsJwtVerify = require('aws-jwt-verify');
 const express = require('express');
-const app = express();
+const jwt = require('aws-jwt-verify');
 const axios = require('axios');
+const cors = require('cors');
 
 
 // Verifier that expects valid access tokens:
@@ -11,9 +11,11 @@ const axios = require('axios');
 //     clientId: "<client_id>",
 //   });
 
+const app = express();
 // Set the port number for the server
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 
 // Define the route for the API call by first validating it
 app.get('/api/validate', async (req, res) => {
@@ -22,10 +24,10 @@ app.get('/api/validate', async (req, res) => {
         if (!authHeader) {
           return res.status(401).send('Unauthorized');
         }
-
+ 
         const token = authHeader.split(' ')[1];
-        const decoded = await awsJwtVerify(token);
-        
+        const decoded = await jwt.verify(token);
+        console.log(token);
         const backendurl = req.headers.backendurl;
         if (!backendurl) {
             return res.status(400).send('Invalid Request');
