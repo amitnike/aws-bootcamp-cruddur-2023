a3790a076ce7a092a7463a6dc3f33e92efaf21f1
Amit <amit.naikwadi84@gmail.com>
2023-03-07 18:45:07 +0000

    jwt verify using separate app

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -31,7 +31,8 @@ export default function HomeFeedPage() {
         span.setAttribute('start_timetamp', Date().toLocaleString());
         const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
         console.log('inside the homefeed1' , backend_url)
-        const auth_backend_url = `${process.env.AUTH_APP_URL}/api/validate`
+        //const auth_backend_url = `${process.env.AUTH_APP_URL}/api/validate`
+        const auth_backend_url = `https://3001-amitnike-awsbootcampcru-bopui05et79.ws-us89b.gitpod.io/api/validate`
         console.log('inside the homefeed2' , auth_backend_url)
         const res = await fetch(auth_backend_url, {
           headers: {
--- node-jwt-validator/server.js
+++ node-jwt-validator/server.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const jwt = require('aws-jwt-verify');
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
 const axios = require('axios');
 const cors = require('cors');
-
-
-// Verifier that expects valid access tokens:
-// const verifier = awsJwtVerify.create({
-//     userPoolId: "<user_pool_id>",
-//     tokenUse: "access",
-//     clientId: "<client_id>",
-//   });
 
 const app = express();
 // Set the port number for the server
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+
+// Verifier that expects valid access tokens:
+const verifier = CognitoJwtVerifier.create({
+    userPoolId: "ap-south-1_HvWBed4Mc",
+    tokenUse: "access",
+    clientId: "4a930ki8237113j3847gpuov0n",
+  });
+
+
+
 
 // Define the route for the API call by first validating it
 app.get('/api/validate', async (req, res) => {
@@ -25,24 +27,28 @@ app.get('/api/validate', async (req, res) => {
           return res.status(401).send('Unauthorized');
         }
  
-        const token = authHeader.split(' ')[1];
-        const decoded = await jwt.verify(token);
-        console.log(token);
-        const backendurl = req.headers.backendurl;
-        if (!backendurl) {
-            return res.status(400).send('Invalid Request');
-        }
-        const url = backendurl.split(' ')[1];
-        
+        const jwtToken = authHeader.split(' ')[1];
+        console.log(jwtToken);
+        const payload = await verifier.verify(jwtToken);
+        console.log(payload);
+        const backendurl = req.headers.BakcendURL;
+        console.log(backendurl);
+        // if (!backendurl) {
+        //     return res.status(400).send('Invalid Request');
+        // }
+        //const url = backendurl.split(' ')[1];
+        const url = `https://4567-amitnike-awsbootcampcru-bopui05et79.ws-us89b.gitpod.io/api/activities/home`
+        console.log(url);
         // If token is valid, make API call with token in Authorization header
         const response = await axios.get(url, {});
-    
+        console.log(response.data);
         return res.send(response.data);
       } catch (error) {
         console.error(error);
         return res.status(401).send('Unauthorized');
       }
 });
+
 
 // Start the server
 app.listen(PORT, () => {
