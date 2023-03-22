eafe727622833c39fc4de7294eeb919063777e65
Amit <amit.naikwadi84@gmail.com>
2023-03-22 23:41:58 +0530

    Check Auth + new page added

--- frontend-react-js/src/pages/MessageGroupNewPage.js
+++ frontend-react-js/src/pages/MessageGroupNewPage.js
@@ -9,6 +9,7 @@ import MessagesForm from '../components/MessageForm';
 import checkAuth from '../lib/CheckAuth';
 
 export default function MessageGroupPage() {
+  const [otherUser, setOtherUser] = React.useState([]);
   const [messageGroups, setMessageGroups] = React.useState([]);
   const [messages, setMessages] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
@@ -16,18 +17,16 @@ export default function MessageGroupPage() {
   const dataFetchedRef = React.useRef(false);
   const params = useParams();
 
-  const loadMessageGroupsData = async () => {
+  const loadUserShortData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/users/@${params.handle}/short`
       const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-          },
         method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setMessageGroups(resJson)
+        console.log('other user:',resJson)
+        setOtherUser(resJson)
       } else {
         console.log(res)
       }
@@ -36,18 +35,18 @@ export default function MessageGroupPage() {
     }
   };  
 
-  const loadMessageGroupData = async () => {
+  const loadMessageGroupsData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
-          },
+        },
         method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setMessages(resJson)
+        setMessageGroups(resJson)
       } else {
         console.log(res)
       }
@@ -63,14 +62,14 @@ export default function MessageGroupPage() {
     dataFetchedRef.current = true;
 
     loadMessageGroupsData();
-    loadMessageGroupData();
+    loadUserShortData();
     checkAuth(setUser);
   }, [])
   return (
     <article>
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <section className='message_groups'>
-        <MessageGroupFeed message_groups={messageGroups} />
+        <MessageGroupFeed otherUser={otherUser} message_groups={messageGroups} />
       </section>
       <div className='content messages'>
         <MessagesFeed messages={messages} />
