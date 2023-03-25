5a7dd8c9c9820deca54e0d010c9c127bfee2a7ea
Amit <amit.naikwadi84@gmail.com>
2023-03-25 19:02:15 +0530

    Minor changes to UI app

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -1,7 +1,7 @@
 import './MessageForm.css';
 import React from "react";
 import process from 'process';
-import { useParams } from 'react-router-dom';
+import { json, useParams } from 'react-router-dom';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -19,10 +19,9 @@ export default function ActivityForm(props) {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
       console.log('onsubmit payload', message)
-
-      let json = { message: message }
+      let json = { 'message': message }
       if (params.handle) {
-        json.user_receiver_handle = params.handle
+        json.handle = params.handle
       } else {
         json.message_group_uuid = params.message_group_uuid
       }
@@ -46,8 +45,8 @@ export default function ActivityForm(props) {
           props.setMessages(current => [...current,data]);
         }
       } else {
-          console.log(res)
-        }
+        console.log(res)
+      }
     } catch (err) {
       console.log(err);
     }
--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -1,6 +1,5 @@
 import './MessageGroupPage.css';
 import React from "react";
-import { useParams } from 'react-router-dom';
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
@@ -10,11 +9,10 @@ import checkAuth from '../lib/CheckAuth';
 
 export default function MessageGroupPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
-  const [messages, setMessages] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
-  const params = useParams();
+
 
   const loadMessageGroupsData = async () => {
     try {
@@ -36,34 +34,12 @@ export default function MessageGroupPage() {
     }
   };  
 
-  const loadMessageGroupData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
-      const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-          },
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setMessages(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };  
-
 
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
-
     loadMessageGroupsData();
-    loadMessageGroupData();
     checkAuth(setUser);
   }, [])
   return (
