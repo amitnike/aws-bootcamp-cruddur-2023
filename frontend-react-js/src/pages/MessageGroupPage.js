430fc8c3b2c76888c0df970d894d8820cac2b15a
Amit <amit.naikwadi84@gmail.com>
2023-03-25 14:23:14 +0000

    code correction to list messages

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -1,5 +1,6 @@
 import './MessageGroupPage.css';
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
@@ -13,6 +14,7 @@ export default function MessageGroupPage() {
   const [popped, setPopped] = React.useState([]);
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
+  const params = useParams();
 
 
   const loadMessageGroupsData = async () => {
@@ -35,12 +37,33 @@ export default function MessageGroupPage() {
     }
   };  
 
+  const loadMessageGroupData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          },
+        method: "GET"
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessages(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
     loadMessageGroupsData();
+    loadMessageGroupData();
     checkAuth(setUser);
   }, [])
   return (
