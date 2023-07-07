ca6889d3d44e92f4fbe92044e853616c3757f683
Amit <amit.naikwadi84@gmail.com>
2023-07-07 20:03:50 +0530

    Refactor the app routes in separate files

--- backend-flask/lib/cloudwatch.py
+++ backend-flask/lib/cloudwatch.py
@@ -3,7 +3,6 @@ import logging
 from flask import request
 
 
-
 def init_cloudwatch(response):
   timestamp = strftime('[%Y-%b-%d %H:%M]')
   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
