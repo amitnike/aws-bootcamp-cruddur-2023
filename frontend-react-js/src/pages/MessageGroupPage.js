660ea45fa736b3cfdea37c1897de075d5428ac15
Amit <amit.naikwadi84@gmail.com>
2023-07-08 10:00:24 +0530

    import added

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom';
 
 import {get} from 'lib/Requests';
 import {checkAuth} from '../lib/CheckAuth';
-import {get} from 'lib/Requests';
 
 import DesktopNavigation  from 'components/DesktopNavigation';
 import MessageGroupFeed from 'components/MessageGroupFeed';
